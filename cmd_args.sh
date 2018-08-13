8738207a37062ba7589cc046a053a016ea84d203
mrmuli <jayjaymuli00@gmail.com>
2018-08-13 21:40:07 +0300

    drop function, coincise case

--- cmd_args.sh
+++ cmd_args.sh
@@ -1,33 +1,23 @@
 #!/usr/bin/env bash
 
+declare -A good
+declare -A bad
 
-autoload -Uz is-at-least
-function cmd_args() {
-
-    for i in "$@"
-    do
-    case $i in
-        -g=* | --good=*)
-        GOOD="${i#*=}"
-        shift
-        ;;
-        -b=* | --bad=*)
-        BAD="${i#*=}"
-        shift
-        ;;
-        --default)
-        DEFAULT=YES
-        shift
-        ;;
-        *)
-
-        ;;
-    esac
-    done
-    echo "GOOD = ${GOOD}"
-    echo "BAD = ${BAD}"
-    if [[ - $1 ]]; then
-        echo "not an argument try again"
-    fi
-}
-
+for i in "$@"
+do
+case $i in
+    -g=* | --good=*)
+    GOOD="${i#*=}"
+    shift
+    ;;
+    -b=* | --bad=*)
+    BAD="${i#*=}"
+    shift
+    ;;
+    *)
+        # unknown option
+    ;;
+esac
+done
+echo "GOOD = ${GOOD}"
+echo "BAD = ${BAD}"
