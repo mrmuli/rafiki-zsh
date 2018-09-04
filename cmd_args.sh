e57ff13ac3bad793d45ee106d398ea93394b4761
mrmuli <jayjaymuli00@gmail.com>
2018-09-04 16:31:35 +0300

    echo input to script

--- cmd_args.sh
+++ cmd_args.sh
@@ -24,12 +24,20 @@ case $i in
 esac
 done
 
+# check for input in arguments (Good or Bad values) 
+# if they are not empty add them to the list (good after [G] bad after [B])
 if [[ -n ${GOOD} ]]; then
-    echo "not empty" ${GOOD}
-    echo good[goodemoji]=${GOOD} >> emoda
+    echo "GOOD value not empty" ${GOOD}
+    for val in $(grep [G] emoda); do
+        # echo good[goodemoji]=${GOOD} >> emoda
+        echo -e "good[goodemoji]=${GOOD}\n$(cat emoda)" > emoda
+    done
 fi
 
 if [[ -n ${BAD} ]]; then
-    echo "not empty" ${BAD}
-    echo bad[bademoji]=${BAD} >> emoda
+    echo "BAD value not empty" ${BAD}
+    for val in $(grep [B] emoda); do
+        #echo bad[bademoji]=${BAD} >> emoda
+        echo -e "bad[bademoji]=${BAD}\n$(cat emoda)" > emoda
+    done
 fi
